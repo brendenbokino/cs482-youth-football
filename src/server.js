9cb3c530f5158e37dd27deff1aacacadd7bb9ca9
lorenkim <lkim1@loyola.edu>
2025-10-20 13:52:03 -0400

    testing output with js

---
+++ src/server.js
@@ -0,0 +1,14 @@
+// Interation #1
+// Due Oct 23, 2025, 11:00 PM
+
+// Loren Kim - 1/2 Coach account
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+console.log("Testing output");
+
+let userInput = {};
