fc58a60990e7adb3dffde89f70f10c2597b4fbb3
lorenkim <lkim1@loyola.edu>
2025-10-21 17:58:13 -0400

    django mvc

---
+++ src/server.test.js
@@ -0,0 +1,11 @@
+// Tests for Iteration #1
+const BasicTest = require('./server');
+const readline = require('readline');
+
+
+test("Simple output", function() {
+    const input = ['Alice', '30'];
+    const test = new BasicTest(input);
+
+    
+});
