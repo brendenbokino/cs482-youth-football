79186460cb76c2adc01f69b4e9267ecd5f6ab439
lorenkim <lkim1@loyola.edu>
2025-10-23 07:29:31 -0400

    Added test coverage (C5)

--- Main.js
+++ Main.js
@@ -2,6 +2,8 @@ const Coach = require('./src/Coach');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+// if running for first time, npm install mongoose dotenv -> Node Main
+
 async function main() {
     const DB_URI = process.env.DB_URI;
     await mongoose.connect(DB_URI);
