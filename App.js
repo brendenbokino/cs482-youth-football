e1f5d5381df8f656d5f26e454f3aba9d56d2d262
oscarroat <oroat@loyola.edu>
2025-10-25 18:54:33 -0400

    get all files function, display to /files

--- App.js
+++ App.js
@@ -22,11 +22,11 @@ var gfs;
 mongoose.connection.once('open', () => {
     //init stream
     gfs = Grid(mongoose.connection.db, mongoose.mongo);
-    gfs.collection('photos');
+    gfs.collection('uploads');
 })
 
 //create storage option
-/**const storage = new GridFsStorage({
+const storage = new GridFsStorage({
     url: process.env.FILESDB_URI,
     file: (req, file) => {
       return new Promise((resolve, reject) => {
@@ -37,22 +37,60 @@ mongoose.connection.once('open', () => {
           const filename = buf.toString('hex') + path.extname(file.originalname);
           const fileInfo = {
             filename: filename,
-            bucketName: 'photos'
+            bucketName: 'uploads'
           };
           resolve(fileInfo);
         });
       });
     }
-});**/
-const storage = new GridFsStorage({url: process.env.FILESDB_URI});
+});
+//const storage = new GridFsStorage({url: process.env.FILESDB_URI});
 const upload = multer({ storage });
 
 
 app.use(express.static('view/html'));
 
-// @route POST /upload
+// POST /upload
 app.post('/upload', upload.single('file'), (req, res) => {
-    res.json({file: req.file});
+    //res.json({file: req.file});
+    res.redirect('/')
 })
 
+// GET /files
+// display all files in JSON
+/**app.get('/files', (req, res) => {
+  try{
+    let files = await gfs.files.find().toArray();
+    return res.json(files);
+  } catch (err) {
+    return res.status(404).json({
+      err: 'no files exist'
+    })
+  }
+  gfs.files.find().toArray((err, files) => {
+    // check if files
+    if(!files || files.length === 0)
+      return res.status(404).json({
+        err: 'no files exist'
+      });
+    
+    //files exist
+    return res.json(files);
+  });
+});**/
+
+/**app.get("/files", async (req, res) => {
+  await gfs.files.find().toArray((err, files) => {
+  if (err) return res.status(400).json({ err });
+  return res.json(files);
+});
+});**/
+app.get("/files", async (req, res) => {
+  try {
+      let files = await gfs.files.find().toArray();
+      res.json({files})
+  } catch (err) {
+      res.json({err})
+  }
+});
 exports.app = app;
