ac1b54906efd4f2a4afd2da68de98bb91c7296a8
lorenkim <lkim1@loyola.edu>
2025-11-06 13:21:03 -0500

    adding more message dao tests (#U2)

--- model/MessageDao.test.js
+++ model/MessageDao.test.js
@@ -1,21 +1,48 @@
-const mongoose = require("mongoose");
-const MessageDao = require("./MessageDao");
+const dbcon = require('./DbConnect');
+const dao = require('./UserDao');
 
-jest.mock("mongoose", () => {
-  const mockModel = {
-    create: jest.fn(),
-    find: jest.fn().mockReturnThis(),
-    sort: jest.fn().mockReturnThis(),
-    lean: jest.fn(),
-    findById: jest.fn(),
-    findByIdAndDelete: jest.fn(),
-    findByIdAndUpdate: jest.fn(),
+beforeAll(async function(){ 
+    await dbcon.connect('test');
+});
+afterAll(async function(){ 
+    await dao.deleteAll();
+    dbcon.disconnect();
+});
+beforeEach(async function(){ 
+    await dao.deleteAll();
+});
+afterEach(function(){
+    //No need
+ 
+});
+
+test('Create new message', async () => {
+  const newMessage = {
+    message: 'Hi there',
+    author: 'Loren',
+    authorType: 1
   };
-  const mockSchema = jest.fn(() => mockModel);
-  const mockMongoose = {
-    Schema: mockSchema,
-    model: jest.fn(() => mockModel),
-  };
-  return mockMongoose;
+
+  const created = await MessageDao.create(newMessage);
+  const found = await MessageDao.findById(created._id);
+
+  expect(created._id).not.toBeNull();
+  expect(found.message).toBe('Hi there');
+  expect(found.author).toBe('Loren');
+});
+
+test('Read all messages', async () => {
+  const msg1 = { message: 'Test 1', author: 'Alice', authorType: 1 };
+  const msg2 = { message: 'Test 2', author: 'Bob', authorType: 2 };
+  const msg3 = { message: 'Test 3', author: 'Carol', authorType: 3 };
+
+  await MessageDao.create(msg1);
+  await MessageDao.create(msg2);
+  await MessageDao.create(msg3);
+
+  const messages = await MessageDao.readAll();
+
+  expect(messages.length).toBe(3);
+  expect(messages[0]).toHaveProperty('message');
 });
 
