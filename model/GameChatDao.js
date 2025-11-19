1e6055abfb55ef057dbabed3d591c767f814ad40
lorenkim <lkim1@loyola.edu>
2025-11-19 14:39:53 -0500

    pasted frontend from comms (#U12)

--- model/GameChatDao.js
+++ model/GameChatDao.js
@@ -1,19 +1,69 @@
 const mongoose = require("mongoose");
 const gameDao = require('./GameDao');
 
+const replySchema = new mongoose.Schema({
+  email: { type: String, required: true },
+  message: { type: String, required: true },
+  date: { type: Date, default: Date.now },
+});
+
 const gameChatSchema = new mongoose.Schema({
   gameId: { type: String, required: true },
-  message: { type: String, required: true },
+  message: { type: String, default: "" }, 
   author: { type: String, required: true },
   authorType: { type: Number, required: true },
+  photo: { type: String }, 
+  video: { type: String }, 
+  edited: { type: Boolean, default: false },
   dateCreated: { type: Date, default: Date.now },
+  dateEdited: { type: Date },
+  replies: [replySchema],
 });
 
 const gameChatModel = mongoose.model('GameChat', gameChatSchema);
 
 module.exports = {
   async create(data) {
-    return await gameChatModel.create(data);
+      return await messageModel.create(data);
+  },
+
+  async readAll() {
+      return await messageModel.find().sort({ date: -1 }).lean();
+  },
+
+  async findById(id) {
+      return await messageModel.findById(id);
+  },
+
+  async addReply(id, reply) {
+      const msg = await messageModel.findById(id);
+      if (!msg) return null;
+      msg.replies.push({ ...reply, date: new Date() });
+      await msg.save();
+      return msg;
+  },
+
+  async addPhoto(id, photoUrl) {
+      const msg = await messageModel.findById(id);
+      if (!msg) return null;
+      msg.photo = photoUrl;
+      await msg.save();
+      return msg;
+  },
+
+  async delete(id) {
+      return await messageModel.findByIdAndDelete(id);
+  },
+
+  async isAuthor(messageId, userName) {
+      const message = await messageModel.findById(messageId);
+      return message && message.author === userName;
+  },
+
+  async update(id, updates) {
+      updates.edited = true;
+      updates.dateEdited = new Date();
+      return await messageModel.findByIdAndUpdate(id, updates, { new: true });
   },
 
   async createForGame(gameId, data) {
@@ -29,13 +79,5 @@ module.exports = {
     }
 
     return await gameChatModel.create({ gameId, ...data });
-  },
-
-  async readByGameId(gameId) {
-    return await gameChatModel.find({ gameId }).sort({ dateCreated: 1 }).lean();
-  },
-
-  async deleteByGameId(gameId) {
-    return await gameChatModel.deleteMany({ gameId });
   },
 };
---
+++ src/GameChat.js
@@ -0,0 +1,37 @@
+// Iteration #3
+// Game Chat
+// Loren Kim
+
+const express = require("express");
+const MessageDao = require('../model/GameChatDao');
+
+class Comms {
+    constructor() {
+        this.currentUser = null;
+    }
+
+    async viewMessages() {
+        if (!this.currentUser) {
+            console.log("Please log in to view messages.");
+            return;
+        }
+
+        const messages = await MessageDao.readAll();
+        if (!messages.length) {
+            console.log("No messages.");
+            return;
+        }
+
+        console.log("\nMessage Board:");
+        messages.forEach((msg, i) => {
+            console.log(`${i + 1}. [${new Date(msg.dateCreated).toLocaleString()}] ${msg.author}: ${msg.message}`);
+            if (msg.replies && msg.replies.length > 0) {
+                msg.replies.forEach((r, j) => {
+                    console.log(`   ↳ Reply ${j + 1} by ${r.email} [${new Date(r.date).toLocaleString()}]: ${r.message}`);
+                });
+            }
+        });
+    }
+}
+
+module.exports = GameChat;
