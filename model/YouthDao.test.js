d03a0775ba58e9c80405a104d051de02b3d44fab
Brenden Bokino <bbokino@loyola.edu>
2025-11-25 14:50:17 -0500

    Made YouthDao testing more concise, improved coverage

--- model/YouthDao.test.js
+++ model/YouthDao.test.js
@@ -2,6 +2,44 @@ const dbcon = require('./DbConnect');
 const youthDao = require('./YouthDao');
 const userDao = require('./UserDao');
 const { default: expect } = require('expect');
+const mongoose = require('mongoose');
+
+// Helper function to create a unique youth user
+function createYouthUser(suffix = '') {
+    return {
+        name: `Youth${suffix}`,
+        password: '123456',
+        permission: 3,
+        username: `youth${suffix}`
+    };
+}
+
+// Helper function to create a unique adult user
+function createAdultUser(suffix = '') {
+    return {
+        name: `Adult${suffix}`,
+        email: `adult${suffix}@test.com`,
+        password: '123456',
+        permission: 1,
+        username: `adult${suffix}`
+    };
+}
+
+// Helper function to create a youth record structure
+function createYouthRecord(userId, adultId, position = 'Running Back', teamId = null) {
+    const record = {
+        id_user: userId,
+        position: position,
+        dob: new Date('2010-05-15'),
+        id_adult: adultId
+    };
+    
+    if (teamId) {
+        record.id_team = teamId;
+    }
+    
+    return record;
+}
 
 beforeAll(async function(){ 
     await dbcon.connect('test');
@@ -19,36 +57,12 @@ beforeEach(async function(){
 });
 
 test('Create new youth test', async function(){
-    // First create a user (youth) and an adult
-    let youthUserData = {
-        name: 'Youth Test',
-        password: '123456',
-        permission: 3,
-        username: 'youthuser',
-    };
-    let adultUserData = {
-        name: 'Adult Test',
-        email: 'adult@test.com',
-        password: '123456',
-        permission: 1,
-        username: 'adultuser',
-        phone: '0987654321'
-    };
+    let createdYouthUser = await userDao.create(createYouthUser());
+    let createdAdultUser = await userDao.create(createAdultUser());
     
-    let createdYouthUser = await userDao.create(youthUserData);
-    let createdAdultUser = await userDao.create(adultUserData);
-    
-    // Now create the youth record
-    let newYouth = {
-        id_user: createdYouthUser._id,
-        position: 'Running Back',
-        dob: new Date('2010-05-15'),
-        id_adult: createdAdultUser._id
-    };
-    
+    let newYouth = createYouthRecord(createdYouthUser._id, createdAdultUser._id);
     let created = await youthDao.create(newYouth);
     
-    // Assertions
     expect(created._id).not.toBeNull();
     expect(created.position).toBe('Running Back');
     expect(created.id_user).toEqual(createdYouthUser._id);
@@ -56,37 +70,13 @@ test('Create new youth test', async function(){
 });
 
 test('Delete Youth', async function(){
-    // Create users first
-    let youthUserData = {
-        name: 'Youth Test',
-        password: '123456',
-        permission: 3,
-        username: 'youthuser',
-    };
-    let adultUserData = {
-        name: 'Adult Test',
-        email: 'adult@test.com',
-        password: '123456',
-        permission: 1,
-        username: 'adultuser',
-        phone: '0987654321'
-    };
+    let createdYouthUser = await userDao.create(createYouthUser());
+    let createdAdultUser = await userDao.create(createAdultUser());
     
-    let createdYouthUser = await userDao.create(youthUserData);
-    let createdAdultUser = await userDao.create(adultUserData);
-    
-    // Create youth
-    let newYouth = {
-        id_user: createdYouthUser._id,
-        position: 'Quarterback',
-        dob: new Date('2011-03-20'),
-        id_adult: createdAdultUser._id
-    };
-    
+    let newYouth = createYouthRecord(createdYouthUser._id, createdAdultUser._id, 'Quarterback');
     let created = await youthDao.create(newYouth);
     let deleted = await youthDao.del(created._id);
     
-    // Try to find deleted youth
     let youths = await youthDao.readAllYouth();
     let found = youths.find(y => y._id.equals(created._id));
     
@@ -95,60 +85,15 @@ test('Delete Youth', async function(){
 });
 
 test('Read All Youths', async function(){
-    // Create adult user
-    let adultUserData = {
-        name: 'Adult Test',
-        email: 'adult@test.com',
-        password: '123456',
-        permission: 1,
-        username: 'adultuser',
-        phone: '0987654321'
-    };
-    let createdAdultUser = await userDao.create(adultUserData);
+    let createdAdultUser = await userDao.create(createAdultUser());
     
-    // Create 3 youth users
-    let youth1Data = {
-        name: 'Youth 1',
-        password: '123456',
-        permission: 3,
-        username: 'youth1',
-    };
-    let youth2Data = {
-        name: 'Youth 2',
-        password: '123456',
-        permission: 3,
-        username: 'youth2',
-    };
-    let youth3Data = {
-        name: 'Youth 3',
-        password: '123456',
-        permission: 3,
-        username: 'youth3',
-    };
+    let youthUser1 = await userDao.create(createYouthUser('1'));
+    let youthUser2 = await userDao.create(createYouthUser('2'));
+    let youthUser3 = await userDao.create(createYouthUser('3'));
     
-    let youthUser1 = await userDao.create(youth1Data);
-    let youthUser2 = await userDao.create(youth2Data);
-    let youthUser3 = await userDao.create(youth3Data);
-    
-    // Create youth records
-    await youthDao.create({
-        id_user: youthUser1._id,
-        position: 'Wide Receiver',
-        dob: new Date('2010-01-10'),
-        id_adult: createdAdultUser._id
-    });
-    await youthDao.create({
-        id_user: youthUser2._id,
-        position: 'Linebacker',
-        dob: new Date('2011-02-15'),
-        id_adult: createdAdultUser._id
-    });
-    await youthDao.create({
-        id_user: youthUser3._id,
-        position: 'Cornerback',
-        dob: new Date('2012-03-20'),
-        id_adult: createdAdultUser._id
-    });
+    await youthDao.create(createYouthRecord(youthUser1._id, createdAdultUser._id, 'Wide Receiver'));
+    await youthDao.create(createYouthRecord(youthUser2._id, createdAdultUser._id, 'Linebacker'));
+    await youthDao.create(createYouthRecord(youthUser3._id, createdAdultUser._id, 'Cornerback'));
     
     let allYouths = await youthDao.readAllYouth();
     
@@ -156,36 +101,11 @@ test('Read All Youths', async function(){
 });
 
 test('Update Youth', async function(){
-    // Create users
-    let youthUserData = {
-        name: 'Youth Test',
-        password: '123456',
-        permission: 3,
-        username: 'youthuser',
-    };
-    let adultUserData = {
-        name: 'Adult Test',
-        email: 'adult@test.com',
-        password: '123456',
-        permission: 1,
-        username: 'adultuser',
-        phone: '0987654321'
-    };
+    let youthUser = await userDao.create(createYouthUser());
+    let adultUser = await userDao.create(createAdultUser());
     
-    let youthUser = await userDao.create(youthUserData);
-    let adultUser = await userDao.create(adultUserData);
+    let created = await youthDao.create(createYouthRecord(youthUser._id, adultUser._id));
     
-    // Create youth
-    let newYouth = {
-        id_user: youthUser._id,
-        position: 'Running Back',
-        dob: new Date('2010-05-15'),
-        id_adult: adultUser._id
-    };
-    
-    let created = await youthDao.create(newYouth);
-    
-    // Update position
     let updates = { position: 'Fullback' };
     let updated = await youthDao.update(created._id, updates);
     
@@ -194,38 +114,10 @@ test('Update Youth', async function(){
 });
 
 test('Find Youths by Adult ID', async function(){
-    // Create adult
-    let adultData = {
-        name: 'Adult',
-        email: 'adult1@test.com',
-        password: '123456',
-        permission: 1,
-        username: 'adult1',
-        phone: '1111111111'
-    };
+    let adult = await userDao.create(createAdultUser('1'));
+    let youthUser = await userDao.create(createYouthUser('1'));
     
-    let adult = await userDao.create(adultData);
-  
-    // Create youth user
-    let youthData = {
-        name: 'Youth',
-        password: '123456',
-        permission: 3,
-        username: 'youth1',
-    };
-
-    
-    let youthUser = await userDao.create(youthData);
-
-    
-    // Create youth
-    await youthDao.create({
-        id_user: youthUser._id,
-        position: 'Safety',
-        dob: new Date('2010-05-10'),
-        id_adult: adult._id
-    });
-
+    await youthDao.create(createYouthRecord(youthUser._id, adult._id, 'Safety'));
     
     let adultYouths = await youthDao.findByAdultId(adult._id);
     
@@ -233,40 +125,11 @@ test('Find Youths by Adult ID', async function(){
     expect(adultYouths[0].position).toBe('Safety');
 });
 
-
 test('Find Youths by User ID', async function(){
-    // Create adult
-    let adultData = {
-        name: 'Adult',
-        email: 'adult1@test.com',
-        password: '123456',
-        permission: 1,
-        username: 'adult1',
-        phone: '1111111111'
-    };
+    let adult = await userDao.create(createAdultUser('1'));
+    let youthUser = await userDao.create(createYouthUser('1'));
     
-    let adult = await userDao.create(adultData);
-  
-    // Create youth user
-    let youthData = {
-        name: 'Youth',
-        password: '123456',
-        permission: 3,
-        username: 'youth1',
-    };
-
-    
-    let youthUser = await userDao.create(youthData);
-
-    
-    // Create youth
-    await youthDao.create({
-        id_user: youthUser._id,
-        position: 'Safety',
-        dob: new Date('2010-05-10'),
-        id_adult: adult._id
-    });
-
+    await youthDao.create(createYouthRecord(youthUser._id, adult._id, 'Safety'));
     
     let youth = await youthDao.findByUserId(youthUser._id);
     
@@ -274,88 +137,45 @@ test('Find Youths by User ID', async function(){
     expect(youth.position).toBe('Safety');
 });
 
-test('Update Youth', async function(){
-    // Create users
-    let youthUserData = {
-        name: 'Youth Test',
-        password: '123456',
-        permission: 3,
-        username: 'youthuser',
-    };
-    let adultUserData = {
-        name: 'Adult Test',
-        email: 'adult@test.com',
-        password: '123456',
-        permission: 1,
-        username: 'adultuser',
-        phone: '0987654321'
-    };
-    
-    let youthUser = await userDao.create(youthUserData);
-    let adultUser = await userDao.create(adultUserData);
-    
-    // Create youth
-    let newYouth = {
-        id_user: youthUser._id,
-        position: 'Running Back',
-        dob: new Date('2010-05-15'),
-        id_adult: adultUser._id
-    };
-    
-    let created = await youthDao.create(newYouth);
-    
-    // Update position
-    let updates = { position: 'Fullback' };
-    let updated = await youthDao.update(created._id, updates);
-    
-    expect(updated.position).toBe('Fullback');
-    expect(updated._id).toEqual(created._id);
-});
-
 test('Check if Youth is Under Adult', async function(){
-    // Create users
-    let youthUserData = {
-        name: 'Youth Test',
-        email: 'youth@test.com',
-        permission: 3,
-        username: 'youthuser',
-        password: '123456'
-    };
-    let adult1Data = {
-        name: 'Adult 1',
-        email: 'adult1@test.com',
-        password: '123456',
-        permission: 1,
-        username: 'adult1',
-        phone: '1111111111'
-    };
-    let adult2Data = {
-        name: 'Adult 2',
-        email: 'adult2@test.com',
-        password: '123456',
-        permission: 1,
-        username: 'adult2',
-        phone: '2222222222'
-    };
+    let youthUser = await userDao.create(createYouthUser());
+    let adult1 = await userDao.create(createAdultUser('1'));
+    let adult2 = await userDao.create(createAdultUser('2'));
     
-    let youthUser = await userDao.create(youthUserData);
-    let adult1 = await userDao.create(adult1Data);
-    let adult2 = await userDao.create(adult2Data);
+    let created = await youthDao.create(createYouthRecord(youthUser._id, adult1._id, 'Center'));
     
-    // Create youth under adult1
-    let newYouth = {
-        id_user: youthUser._id,
-        position: 'Center',
-        dob: new Date('2010-08-25'),
-        id_adult: adult1._id
-    };
-    
-    let created = await youthDao.create(newYouth);
-    
-    // Check relationships
     let isUnderAdult1 = await youthDao.isYouthUnderAdult(created._id, adult1._id);
     let isUnderAdult2 = await youthDao.isYouthUnderAdult(created._id, adult2._id);
     
     expect(isUnderAdult1).toBe(true);
     expect(isUnderAdult2).toBe(false);
+});
+
+test('Read Youth by ID', async function(){
+    let youthUser = await userDao.create(createYouthUser());
+    let adultUser = await userDao.create(createAdultUser());
+    
+    let created = await youthDao.create(createYouthRecord(youthUser._id, adultUser._id, 'Tight End'));
+    
+    let found = await youthDao.read(created._id);
+    
+    expect(found).not.toBeNull();
+    expect(found._id).toEqual(created._id);
+    expect(found.position).toBe('Tight End');
+});
+
+test('Get Youths on Team', async function(){
+    const teamId = new mongoose.Types.ObjectId();
+    
+    let adult = await userDao.create(createAdultUser());
+    
+    let youthUser1 = await userDao.create(createYouthUser('1'));
+    let youthUser2 = await userDao.create(createYouthUser('2'));
+    
+    await youthDao.create(createYouthRecord(youthUser1._id, adult._id, 'Forward', teamId));
+    await youthDao.create(createYouthRecord(youthUser2._id, adult._id, 'Guard', teamId));
+    
+    let teamYouths = await youthDao.getYouthOnTeam(teamId);
+    
+    expect(teamYouths.length).toBe(2);
 });
