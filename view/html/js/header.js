d996444c744df48f41b8e1d12ed5d39f9cd3f260
oscarroat <oroat@loyola.edu>
2025-12-04 14:33:42 -0500

    changed spike drop down to admin drop down

--- view/html/js/header.js
+++ view/html/js/header.js
@@ -4,9 +4,9 @@ async function checkLoggedUser(){
     let user =  await response.json();   
 
     if (user != null) {
-        if (user.permission >= 1 && user.permission <= 3) { // User is an adult, coach, or youth
+        /**if (user.permission >= 1 && user.permission <= 3) { // User is an adult, coach, or youth
             document.getElementById("team-page").style.display = "inherit";
-        } 
+        }*/
 
         if (user.permission == 0) { // User is an admin
             document.getElementById("admin-page").style.display = "inherit";
